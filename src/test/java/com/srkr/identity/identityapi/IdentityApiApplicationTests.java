70258961b47eaa0006994635daf2393237128c65
anjaneyadatla <anjaneyalndatla@gmail.com>
2018-09-20 19:27:57 -0400

    Initial commit

---
+++ src/test/java/com/srkr/identity/identityapi/IdentityApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srkr.identity.identityapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdentityApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
